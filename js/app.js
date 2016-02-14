e04307806914e355fc9149d0d9320dcdc548bcfb
plaus <plaus@dhcp-v880-11-00415.1Xwireless.unc.edu>
2016-02-14 18:23:37 -0500

    Added Jquerry library and bootstrap javascript

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 var app = angular.module('myApp', []);
+    
+    app.controller('BaseController', ['$http', function ($http) {
 
-app.controller('BaseController', function() {
+    this.senateData = [ ];
+    var _this = this;
+    
+    $http.get('js/senateinfo.json')
+        .success(function(data) {
+            _this.senateData = data;
+            console.log(data);
+        })
+        .error(function(msg) {
+            console.log("Error");
+        })
 
-    this.message = "Ready";
-
-});
+}]);
